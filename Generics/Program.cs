1b8174c197dd8bcf62b706e6409628decb803b03
Nepolean23 <nlean1996@gmail.com>
2023-01-17 16:32:54 +0530

    [NEPOLEAN]-Code for Finding Maximum Strings

--- Generics/Program.cs
+++ Generics/Program.cs
@@ -4,4 +4,7 @@
 //FindMaximumIntegersNumbers.MaximumIntegerNumbers(10, 20, 30);
 
 //finding of maximum float numbers
-FindMaximumFloatNumbers.MaximumFloatNumbers(1.1,1.2,1.3);
+//FindMaximumFloatNumbers.MaximumFloatNumbers(1.1,1.2,1.3);
+
+//finding of maximum string numbers
+FindMaximumStringNumbers.MaximumString("Apple","Peach","Banana");
